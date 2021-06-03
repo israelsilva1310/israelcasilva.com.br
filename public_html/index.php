e6901bc524ba763d066c0947bf943c90a567c549
Israel Silva <israelsilva1310@gmail.com>
2021-06-03 18:11:35 -0300

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Ola Israel C A Silva</title>
+  </head>
+  <body>
+	<?php
+
+// Mostra todas as informações, usa o padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações dos módulos.
+// phpinfo(8) mostra um resultado identico.
+phpinfo(INFO_MODULES);
+
+?>
+  </body>
+</html>
